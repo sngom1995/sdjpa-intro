abe3988b21a9f4a447b97436a7dfd1af917f4efb
sngom1995 <ngomsamba75@gmail.com>
2023-07-13 13:00:59 +0000

    jpa slice testing

--- src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,10 +1,24 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
+
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void bookRepositoryTest() {
+        Long count = bookRepository.count();
+        assertThat(count).isEqualTo(2);
+    }
 
     @Test
     void contextLoads() {
---
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaSliceTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.sdjpaintro;
+
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repository.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
+public class SpringBootJpaSliceTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Commit
+    @Order(1)
+    @Test
+    void bookRepositoryTestJpa() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+
+        bookRepository.save(new Book(3L,"MyBook", "124326","123"));
+
+        long countAfter = bookRepository.count();
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testTransactionJpa() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(3);
+    }
+}
