ce4d649429d40055d029607579d43cbbf4a85fc9
sngom1995 <ngomsamba75@gmail.com>
2023-07-13 00:02:08 +0000

    book entity, repository

---
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitialization.java
@@ -0,0 +1,28 @@
+package guru.springframework.sdjpaintro.bootstrap;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitialization implements CommandLineRunner {
+    private final BookRepository bookRepository;
+
+    public DataInitialization(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book book1 = new Book(1L, "Spring Jpa","123121","SAMBA");
+        bookRepository.save(book1);
+        Book book2 = new Book(2L, "Spring Framework","123517", "SAMBA");
+        bookRepository.save(book2);
+
+        bookRepository.findAll().forEach(book -> {
+            System.out.println("id: " + book.getId());
+            System.out.println("Title:"+book.getTitle());
+        });
+    }
+}
